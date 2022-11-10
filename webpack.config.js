566e8cb742cd78e62500b79f2fe4bb54e88ebce6
Slavik <charon.type.4@gmail.com>
2022-11-10 22:58:21 +0200

    Comment

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'js'),
+    mode: 'development',
+    entry:'.js/script.js',
+    output:{
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'app'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 3000,
+    }
+}
