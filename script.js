3361ceadd790e81296399998d4da0c012069c7fd
Slavik <charon.type.4@gmail.com>
2022-11-10 22:38:25 +0200

    Comment

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load',function(){
+
+      $('#menu').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+
+      if($(window).scrollTop() > 0){
+        $('.scroll-top').show();
+      }else{
+        $('.scroll-top').hide();
+      }
+
+     
+
+      
+  
+
+    });
+
+    // smooth scrolling
+
+    $('a[href*="#"]').on('click',function(e){
+
+      e.preventDefault();
+
+      $('html, body').animate({
+
+        scrollTop : $($(this).attr('href')).offset().top,
+
+      },
+      500,
+      'linear'
+      )
+
+    })
+
+});
